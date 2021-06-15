aa81d9669d19cbba820ff8e938cb1f2fd45b3fae
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-06-15 22:54:40 +0900

    feat: add sample

---
+++ androidApp/src/main/java/jp/kaleidot725/counter/android/MainActivity.kt
@@ -0,0 +1,44 @@
+package jp.kaleidot725.counter.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import jp.kaleidot725.counter.shared.MyCounter
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val counter = MyCounter(min = 0, max = 100)
+
+        setContent {
+            var count by remember { mutableStateOf(counter.value) }
+
+            Column {
+                Text(text = count.toString())
+
+                Button(onClick = {
+                    counter.plus()
+                    count = counter.value
+                }) {
+                    Text(text = "PLUS")
+                }
+
+                Button(onClick = {
+                    counter.minus()
+                    count = counter.value
+                }) {
+                    Text(text = "MINUS")
+                }
+            }
+        }
+    }
+}
