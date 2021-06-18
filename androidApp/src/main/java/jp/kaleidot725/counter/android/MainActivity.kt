8a459214d991c5a76d7939f23c7dc66b1d5200c0
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-06-18 21:33:39 +0900

    feat: update android layout

--- androidApp/src/main/java/jp/kaleidot725/counter/android/MainActivity.kt
+++ androidApp/src/main/java/jp/kaleidot725/counter/android/MainActivity.kt
@@ -3,7 +3,7 @@ package jp.kaleidot725.counter.android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.mutableStateOf
@@ -11,6 +11,11 @@ import androidx.compose.runtime.remember
 import jp.kaleidot725.counter.shared.MyCounter
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 val counter = MyCounter(min = 0, max = 100)
 
@@ -20,21 +25,39 @@ class MainActivity : AppCompatActivity() {
         setContent {
             var count by remember { mutableStateOf(counter.value) }
 
-            Column {
-                Text(text = count.toString())
+            Box(modifier = Modifier.fillMaxSize()) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .align(Alignment.Center)
+                ) {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = count.toString(),
+                        fontSize = 32.sp,
+                        textAlign = TextAlign.Center
+                    )
 
-                Button(onClick = {
-                    counter.plus()
-                    count = counter.value
-                }) {
-                    Text(text = "PLUS")
-                }
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {
+                            counter.plus()
+                            count = counter.value
+                        }
+                    ) {
+                        Text(text = "PLUS")
+                    }
 
-                Button(onClick = {
-                    counter.minus()
-                    count = counter.value
-                }) {
-                    Text(text = "MINUS")
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {
+                            counter.minus()
+                            count = counter.value
+                        }
+                    ) {
+                        Text(text = "MINUS")
+                    }
                 }
             }
         }
